f154e5e6e15fe9810eea71573548f172b225d41f
Torsten Anders <torsten.anders@beds.ac.uk>
2017-08-12 21:43:12 +0200

    Substantial and working test added

--- tests/first-tests.lisp
+++ tests/first-tests.lisp
@@ -13,7 +13,7 @@
 ;;; *************************************************************
 
 ; (require :ta-utilities)
-(require :tot)
+; (require :tot)
 ; (asdf:load-system :tot)
 (require :cluster-rules)
 
@@ -23,6 +23,7 @@
 (use-package :cluster-engine)
 (use-package :cluster-rules)
 |#
+
 
 
 ;;;;;;;;;;;;;;;;;;;;;
@@ -36,7 +37,7 @@
 
 ;; minimal demo example
 (preview-cluster-engine-score
- (cr::cluster-engine
+ (cr:cluster-engine
   20 
   ;; rules
   (ce::rules->cluster (cr::no-syncopation :metric-structure :beats)
@@ -48,12 +49,152 @@
 
 ;;; BUG: metric domain of multiple values seemingly always results in static meter
 (preview-cluster-engine-score
- (cr::cluster-engine 
+ (cr:cluster-engine 
   20 
   () ; no rules
   '((3 4) (2 4)) 
   '(((1/4) (1/8))
     ((60))))
  )
+
+
+;;;
+;;; Profile test
+;;;
+
+;; cr:follow-profile-hr
+
+(setf my-melody '((q c4 cs4 stacc fs4 -q) 
+                  (e f4 b4 tr2 c4 -h e g4) 
+                  (q d4 e4 f4 g4) 
+                  (h a4 -h)))
+
+;; OK: follow pitch sequence of input melody results in same melody
+;; ... though solution is repeating melody
+(preview-cluster-engine-score
+ (cr:cluster-engine 
+  (count-notes my-melody)
+  ;; rules
+  (ce::rules->cluster 
+   (cr:follow-profile-hr my-melody :voices 0 :mode :pitch))
+  '((4 4)) 
+  `((,(flatten (omn :length my-melody))) ; set rhythm as rhythmic motif
+    ,(mclist (gen-integer 60 84))))
+ )
+
+
+;; OK: test args :n and :start 
+(preview-cluster-engine-score
+ (cr:cluster-engine 
+  20 
+  ;; rules
+  (ce::rules->cluster 
+   (cr:follow-profile-hr my-melody :voices 0 :mode :pitch :n 4 :start 1)
+   )
+  '((4 4)) 
+  `((,(flatten (omn :length my-melody)))
+    ,(mclist (gen-integer 60 84))))
+ )
+
+
+;;; BUG: rhythm profile not working
+(preview-cluster-engine-score
+ (cr:cluster-engine 
+  20 
+  ;; rules
+  (ce::rules->cluster 
+   (cr:follow-profile-hr my-melody :mode :rhythm)
+   )
+  '((4 4)) 
+  '(((1/2) (1/4) (1/8) (-1/2) (-1/4) (-1/8))
+    ((60))))
+ )
+
+
+
+;; whole-tone ascending scale
+(setf my-profile '(q c4 d4 e4 fs4 gs4 as4 c5 d5))
+
+;; OK: using :constrain :intervals 
+(preview-cluster-engine-score
+ (cr:cluster-engine 
+  20 
+  ;; rules
+  (ce::rules->cluster 
+   (cr:follow-profile-hr my-profile :voices 0 :mode :pitch :constrain :intervals)
+   )
+  '((4 4)) 
+  `((,(flatten (omn :length my-profile)))
+    ,(mclist (gen-integer 60 84))))
+ )
+
+
+;;;
+;;; More complex example harmonically transforming a melody (OMN expression) -- ported from Cinderella harmony patch
+;;;
+
+;; Input melody to revise -- replace with whatever melody
+(setf galliard-mel 
+      '((q e5 f slap+stacc -h q eb5 slap+stacc -h) 
+        (q cs5 f slap+stacc eb5 mp ord f5) (q. g5 e gs5 q c6) (h bb5 q a5) (q fs5 - e d5 cs5) (h a5 q b5) (q. cs5 e bb5 q d6) (q cs6 d6 g5) (h e5 q cs6) (q g5 f slap+stacc a5 mp ord bb5) (q. c6 e cs6 q c6) (e b5 a5 q f5 e5) (h b5 q c6) (q eb5 f slap+stacc -e b5 mp ord d6 cs6) (q. eb6 e b5 q e5) (q gs5 d6 b5) (h gs5 q fs5)))
+
+(setf galliard-harmonies
+      '((h. c4eb4g4a4)
+        (w. c4eb4g4a4)        
+        (w. f4g4a4c4) (w. cs4f4g4b4) (w. b4eb4g4a4) (w. f4a4c4eb4) (w. eb4g4b4cs4) (w. a4cs4eb4g4) (w. cs4f4g4c4) (h. g4b4cs4f4)))
+
+(setf galliard-harmonic-rhythm
+      (flatten (omn :length galliard-harmonies)))
+
+;; test
+(preview-score  
+ (list :flt galliard-mel
+       :harm galliard-harmonies))
+
+;; ... solution is repeating melody
+(preview-cluster-engine-score
+ (let ((time-sigs (PWGL-time-signatures 
+                   (get-time-signature galliard-harmonies))))
+   (cr:cluster-engine 
+    (count-notes galliard-mel)
+    ;; rules
+    (let ((mel-voice 2))
+    (ce::rules->cluster 
+     (cr:follow-profile-hr 
+      galliard-mel :voices mel-voice :mode :pitch :constrain :profile)
+     (cr:follow-profile-hr 
+      galliard-mel :voices mel-voice :mode :pitch :constrain :intervals)
+     (ce:r-predefine-meter time-sigs)
+     ;; more rules
+     (cr:only-scale-pcs :voices mel-voice :input-mode :all 
+                        :scale-voice 0)
+     (cr:only-chord-pcs :voices mel-voice :input-mode :beat ; :1st-beat
+                        :chord-voice 1) 
+     (cr:long-notes-chord-pcs :voices mel-voice :max-nonharmonic-dur 1/4)
+     (cr:stepwise-non-chord-tone-resolution
+      :voices mel-voice :input-mode :all :step-size 3)
+     (cr:chord-tone-before/after-rest :voices mel-voice :input-mode :all)
+     (cr:chord-tone-follows-non-chord-tone :voices mel-voice :input-mode :all)
+     (cr:no-repetition :voices mel-voice :window 3)
+     (cr:resolve-skips :voices mel-voice :resolution-size 4)
+     ))
+    ;; meter domain
+    (remove-duplicates time-sigs :test #'equal) 
+    ;; voice domains
+    `(
+      ; scale rhythm
+      (,(length-merge galliard-harmonic-rhythm))
+      ; scale pitches
+      (,(pitch-to-midi '((c4 cs4 ds4 f4 g4 a4 b4))))
+      ; harmonic rhythm for chords 
+      (,galliard-harmonic-rhythm)
+      ; harmony: chords
+      (,(pitch-to-midi
+         (flatten (omn :pitch galliard-harmonies))))
+      ; voice rhythm (input rhythm as motif) 
+      (,(flatten (omn :length galliard-mel))) 
+      ,(mclist (gen-integer 60 84))
+      ))
+   ))
 
 
