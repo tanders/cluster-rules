125c328564f42b522d581a364ff8199ec3692b5a
Torsten Anders <torsten.anders@beds.ac.uk>
2017-10-09 17:19:51 +0100

    Added further tests

--- tests/first-tests.lisp
+++ tests/first-tests.lisp
@@ -198,3 +198,170 @@
       ))
    ))
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Refactoring of example above, this time with a function
+;;;
+
+(setf galliard-mel 
+      '((q e5 f slap+stacc -h q eb5 slap+stacc -h) 
+        (q cs5 f slap+stacc eb5 mp ord f5) (q. g5 e gs5 q c6) (h bb5 q a5) (q fs5 - e d5 cs5) (h a5 q b5) (q. cs5 e bb5 q d6) (q cs6 d6 g5) (h e5 q cs6) (q g5 f slap+stacc a5 mp ord bb5) (q. c6 e cs6 q c6) (e b5 a5 q f5 e5) (h b5 q c6) (q eb5 f slap+stacc -e b5 mp ord d6 cs6) (q. eb6 e b5 q e5) (q gs5 d6 b5) (h gs5 q fs5)))
+
+(setf galliard-harmonies
+      '((h. c4eb4g4a4)
+        (w. c4eb4g4a4)        
+        (w. f4g4a4c4) (w. cs4f4g4b4) (w. b4eb4g4a4) (w. f4a4c4eb4) (w. eb4g4b4cs4) (w. a4cs4eb4g4) (w. cs4f4g4c4) (h. g4b4cs4f4)))
+
+(setf galliard-scales
+      (omn-replace :pitch (gen-repeat (length galliard-harmonies)
+                                      (chordize '(c4 cs4 ds4 f4 g4 a4 b4)))
+                   (omn :length galliard-harmonies)))
+
+(defun harmonise-melody (scales harmonies melody)
+  "[CSP] Revises pitches of melody so that it fits into underlying harmony. Returns cluster-engine score encoding with underlying scale as first part, underlying harmony as second part, and revised melody as third part. 
+  
+  :scales: underlying scales as nested OMN expression, expressing both the underlying scale pitches and their rhythm
+  :harmonies: harmonies as nested OMN expression, expressing both the underlying chords and the harmonic rhythm
+  :melody: melody to harmonically revise as nested OMN expression
+
+  In the current definition, the pitches of the scales and harmonies are untouched in the CSP, i.e., conflicts between thme are not resolved. 
+  "
+ (let ((time-sigs (PWGL-time-signatures 
+                   (get-time-signature melody)))
+       (scale-rhythm (flatten (omn :length scales)))
+       (harmonic-rhythm (flatten (omn :length harmonies))))
+   (cr:cluster-engine 
+    (count-notes melody)
+    ;; rules
+    (let ((mel-voice 2))
+    (ce::rules->cluster 
+     (cr:follow-profile-hr 
+      melody :voices mel-voice :mode :pitch :constrain :profile)
+     (cr:follow-profile-hr 
+      melody :voices mel-voice :mode :pitch :constrain :intervals)
+     (ce:r-predefine-meter time-sigs)
+     ;; more rules
+     (cr:only-scale-pcs :voices mel-voice :input-mode :all 
+                        :scale-voice 0)
+     (cr:only-chord-pcs :voices mel-voice :input-mode :beat ; :1st-beat
+                        :chord-voice 1) 
+     (cr:long-notes-chord-pcs :voices mel-voice :max-nonharmonic-dur 1/4)
+     (cr:stepwise-non-chord-tone-resolution
+      :voices mel-voice :input-mode :all :step-size 3)
+     (cr:chord-tone-before/after-rest :voices mel-voice :input-mode :all)
+     (cr:chord-tone-follows-non-chord-tone :voices mel-voice :input-mode :all)
+     (cr:no-repetition :voices mel-voice :window 3)
+     (cr:resolve-skips :voices mel-voice :resolution-size 4)
+     ))
+    ;; meter domain
+    (remove-duplicates time-sigs :test #'equal) 
+    ;; voice domains
+    `(
+      ; scale rhythm
+      (,scale-rhythm)
+      ; scale pitches
+      (,(pitch-to-midi (flatten (omn :pitch scales))))
+      ; harmonic rhythm for chords 
+      (,harmonic-rhythm)
+      ; harmony: chordsles 
+      (,(pitch-to-midi (flatten (omn :pitch harmonies))))
+      ; melody rhythm (input rhythm as motif) 
+      (,(flatten (omn :length melody))) 
+      ; melody pitch domain
+      ,(mclist (apply #'gen-integer (pitch-to-midi (find-ambitus melody :type :pitch))))
+      ))
+   ))
+
+
+(preview-cluster-engine-score
+ (harmonise-melody galliard-scales galliard-harmonies galliard-mel))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Refactoring of example above: allow for multiple polyphonic parts to be revised 
+;;;
+;;; See also function revise-score-harmonically in tot/sources/constraints.lisp
+;;;
+
+
+(defun harmonise-parts (scales harmonies &rest parts)
+  "[CSP] Revises pitches of given parts so that they fit into the given harmony. Returns cluster-engine score encoding with underlying scale as first part, underlying harmony as second part, and all actual parts following that. 
+  
+  :scales: underlying scales as nested OMN expression, expressing both the underlying scale pitches and their rhythm
+  :harmonies: harmonies as nested OMN expression, expressing both the underlying chords and the harmonic rhythm
+  :parts: parts to harmonically revise as nested OMN expression. Note the maximum number of parts is limited by Cluster Engine (to 8 at most?)
+
+  In the current definition, the pitches of the scales and harmonies are untouched in the CSP, i.e., conflicts between them are not resolved. 
+
+  NOTE: Number of notes generated (and resulting meter) takes only first part into account.
+  "
+ (let ((time-sigs (PWGL-time-signatures 
+                   (get-time-signature (first parts))))
+       (scale-rhythm (flatten (omn :length scales)))
+       (harmonic-rhythm (flatten (omn :length harmonies))))
+   (cr:cluster-engine  
+    (count-notes (first parts))
+    ;; all rules
+    (ce::rules->cluster 
+     (mappend #'(lambda (part-n-pos)
+                  (let ((part (first part-n-pos))
+                        (part-pos (second part-n-pos)))
+                    (ce::rules->cluster
+                     ;; profile rules
+                     (cr:follow-profile-hr 
+                      part :voices part-pos :mode :pitch :constrain :profile)
+                     (cr:follow-profile-hr 
+                      part :voices part-pos :mode :pitch :constrain :intervals)
+                     ;; more rules
+                     (cr:only-scale-pcs :voices part-pos :input-mode :all 
+                                        :scale-voice 0)
+                     (cr:only-chord-pcs :voices part-pos :input-mode :beat ; :1st-beat
+                                        :chord-voice 1) 
+                     (cr:long-notes-chord-pcs :voices part-pos :max-nonharmonic-dur 1/4)
+                     (cr:stepwise-non-chord-tone-resolution
+                      :voices part-pos :input-mode :all :step-size 3)
+                     (cr:chord-tone-before/after-rest :voices part-pos :input-mode :all)
+                     (cr:chord-tone-follows-non-chord-tone :voices part-pos :input-mode :all)
+                     (cr:no-repetition :voices part-pos :window 3)
+                     (cr:resolve-skips :voices part-pos :resolution-size 4)                  
+                    )))
+              (tu:mat-trans 
+               (list parts
+                     (gen-integer 2 (1+ (length parts))) ; part positions in score
+                     )))
+     (ce:r-predefine-meter time-sigs))
+    ;; meter domain
+    (remove-duplicates time-sigs :test #'equal)
+    ;; other domains
+    `(
+      ; scale rhythm 
+      (,scale-rhythm)
+      ; scale pitches
+      (,(pitch-to-midi (flatten (omn :pitch scales))))
+      ; harmonic rhythm for chords 
+      (,harmonic-rhythm)
+      ; harmony: chordsles 
+      (,(pitch-to-midi (flatten (omn :pitch harmonies))))
+      ;; actual parts' domains
+      ,@(mappend #'(lambda (part)
+                    `(
+                      ; part's rhythm (input rhythm as motif) 
+                      (,(flatten (omn :length part)))
+                      ; part's pitch domain
+                      ,(mclist (apply #'gen-integer (pitch-to-midi (find-ambitus part :type :pitch)))))
+                    )
+                parts)
+      ))))
+
+
+(preview-cluster-engine-score
+ (harmonise-parts galliard-scales galliard-harmonies galliard-mel))
+
+(preview-cluster-engine-score
+ (harmonise-parts galliard-scales galliard-harmonies galliard-mel (cons '(-h) galliard-mel)))
+
+
+
