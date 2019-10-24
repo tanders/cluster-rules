ffc4b3efb69058a4c0ad363c816d383d8dc57856
Torsten Anders <torsten@aiva.ai>
2019-10-24 14:02:36 +0200

    cluster-engine/test-utils now used by cluster-rules/tests package

--- tests/unit-tests/package.lisp
+++ tests/unit-tests/package.lisp
@@ -1,6 +1,6 @@
 ;;; -*- Mode:Lisp; Syntax:ANSI-Common-Lisp; -*-
 
 (defpackage #:cluster-rules/tests
-  (:use #:cl #:cluster-rules #:FiveAM)
+  (:use #:cl #:cluster-rules #:FiveAM #:cluster-engine/test-utils)
   (:export #:run!
 	   #:all-tests))
